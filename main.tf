6603f15e6c0a944ea6bd3c2c1f793e794e46e472
satishchhatpar <satish.chhatpar@gmail.com>
2021-06-12 08:53:38 +0530

    committing.....

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ resource "aws_instance" "web" {
   ami           = "ami-a0cfeed8"
   instance_type = "t2.micro"
   user_data     = file("init-script.sh")
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   tags = {
     Name = random_pet.name.id
@@ -23,4 +24,27 @@ output "domain-name" {
 
 output "application-url" {
   value = "${aws_instance.web.public_dns}/index.php"
+}
+
+
+
+resource "aws_security_group" "web-sg" {
+  description = "Allow 80 inbound traffic"
+  name = "${random_pet.name.id}-sg"
+
+  ingress {
+    description      = "80 from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
 }
